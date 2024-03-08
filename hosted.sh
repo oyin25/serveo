c040f22540b3ddc962f735d2e61126baaa1d1c22
Hosted Biz <38610254+oyin25@users.noreply.github.com>
2024-03-08 13:01:20 +0100

    Update hosted.sh

--- hosted.sh
+++ hosted.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # hosted.sh
 
+CURRENT_VERSION="1.0"
+
 # Function to display help information
 show_help() {
   echo "Usage: ./hosted.sh [command] [args]"
@@ -8,6 +10,16 @@ show_help() {
   echo "  update              Update this script from GitHub"
   echo "  [local-port] [remote-port] [subdomain(optional)]  Set up a reverse SSH tunnel with serveo.net"
   echo "  help                Display this help message"
+}
+
+# Function to check for updates
+check_for_updates() {
+    # Fetch the latest version number from the GitHub repository
+    LATEST_VERSION=$(curl -s https://raw.githubusercontent.com/oyin25/serveo/main/version.txt)
+    
+    if [ "$LATEST_VERSION" != "$CURRENT_VERSION" ]; then
+        echo -e "\e[31mUpdate available: Version $LATEST_VERSION is available. Run './hosted.sh update' to update.\e[0m"
+    fi
 }
 
 # Handle different commands
@@ -21,6 +33,7 @@ case "$1" in
     show_help
     ;;
   *)
+    check_for_updates
     if [[ -n $1 ]] && [[ -n $2 ]]; then
       # Check if the subdomain argument is provided
       if [[ -n $3 ]]; then
