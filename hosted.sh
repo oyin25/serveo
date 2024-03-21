261890e346c782242c250678edd114df209e8998
Hosted Biz <38610254+oyin25@users.noreply.github.com>
2024-03-21 18:35:30 +0100

    Update hosted.sh

--- hosted.sh
+++ hosted.sh
@@ -5,13 +5,11 @@ CURRENT_VERSION="1.1"
 
 # Function to display help information
 show_help() {
-  echo "Usage: ./hosted.sh -p|-t [remote-port] [local-port] [subdomain(optional)]"
-  echo "Options:"
-  echo "  -p                         Use autossh for a permanent, resilient connection"
-  echo "  -t                         Use ssh for a temporary connection"
+  echo "Usage: ./hosted.sh [local-port] [remote-port] [subdomain] [mode]"
   echo "Commands:"
-  echo "  update                     Update this script from GitHub"
-  echo "  help                       Display this help message"
+  echo "  update              Update this script from GitHub"
+  echo "  [local-port] [remote-port] [subdomain(optional)] [mode: ssh/autossh(optional)]  Set up a reverse SSH tunnel with serveo.net"
+  echo "  help                Display this help message"
 }
 
 # Function to check for updates
@@ -24,54 +22,33 @@ check_for_updates() {
     fi
 }
 
-ssh_command=""
-option_passed=false
-
-# Parse options
-while getopts ":pt" opt; do
-  case ${opt} in
-    p )
-      ssh_command="autossh -M 0"
-      option_passed=true
-      ;;
-    t )
-      ssh_command="ssh"
-      option_passed=true
-      ;;
-    \? )
-      echo "Invalid option: $OPTARG" 1>&2
-      show_help
-      exit 1
-      ;;
-  esac
-done
-
-# Remove the options from the arguments list
-shift $((OPTIND -1))
-
-# Validate if either -p or -t is provided
-if [ "$option_passed" = false ]; then
-  echo "You must specify either -p for a permanent connection or -t for a temporary connection."
-  show_help
-  exit 1
-fi
-
 # Handle different commands
-if [[ $# -ge 2 ]]; then
-  remote_port=$1
-  local_port=$2
-  subdomain=${3:-""}
-  
-  # Construct the remote forwarding part of the SSH command
-  forwarding_spec="$remote_port:localhost:$local_port"
-  if [[ -n $subdomain ]]; then
-    # If subdomain is provided, prefix it to the forwarding specification
-    forwarding_spec="$subdomain:$forwarding_spec"
-  fi
-  
-  # Execute the SSH or autossh command
-  $ssh_command -R $forwarding_spec serveo.net
-else
-  echo "Error: Incorrect usage."
-  show_help
-fi
+case "$1" in
+  update)
+    cd "$(dirname "$0")" && git pull https://github.com/oyin25/serveo.git main
+    echo "Script updated successfully!"
+    ;;
+  help)
+    show_help
+    ;;
+  *)
+    check_for_updates
+    if [[ -n $1 ]] && [[ -n $2 ]]; then
+      command="ssh"
+      if [[ $4 == "autossh" ]]; then
+        command="autossh -M 0"
+      fi
+
+      if [[ -n $3 ]]; then
+        # If the subdomain is provided.
+        $command -R $3:$1:localhost:$2 serveo.net
+      else
+        # If no subdomain is provided.
+        $command -R $1:localhost:$2 serveo.net
+      fi
+    else
+      echo "Error: Incorrect usage."
+      show_help
+    fi
+    ;;
+esac
