df7f3d04f73bcaa9da6467c2147f8d74e6cf64fc
Hosted Biz <38610254+oyin25@users.noreply.github.com>
2024-03-21 18:07:51 +0100

    Update hosted.sh

--- hosted.sh
+++ hosted.sh
@@ -5,11 +5,13 @@ CURRENT_VERSION="1.1"
 
 # Function to display help information
 show_help() {
-  echo "Usage: ./hosted.sh [command] [args]"
+  echo "Usage: ./hosted.sh [options] [remote-port] [local-port] [subdomain(optional)]"
+  echo "Options:"
+  echo "  -p                         Use autossh for a permanent, resilient connection"
+  echo "  -t                         Use ssh for a temporary connection"
   echo "Commands:"
-  echo "  update              Update this script from GitHub"
-  echo "  [local-port] [remote-port] [subdomain(optional)]  Set up a reverse SSH tunnel with serveo.net"
-  echo "  help                Display this help message"
+  echo "  update                     Update this script from GitHub"
+  echo "  help                       Display this help message"
 }
 
 # Function to check for updates
@@ -22,31 +24,56 @@ check_for_updates() {
     fi
 }
 
+ssh_command=""
+
+# Parse options
+while getopts ":pt" opt; do
+  case ${opt} in
+    p )
+      ssh_command="autossh -M 0"
+      ;;
+    t )
+      ssh_command="ssh"
+      ;;
+    \? )
+      echo "Invalid option: $OPTARG" 1>&2
+      show_help
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND -1))
+
+# Validate if either -p or -t is provided
+if [ -z "$ssh_command" ]; then
+  echo "You must specify either -p for a permanent connection or -t for a temporary connection."
+  show_help
+  exit 1
+fi
+
 # Handle different commands
-case "$1" in
-  update)
-    # Navigate to the script's directory
-    cd "$(dirname "$0")" && git pull https://github.com/oyin25/serveo.git main
-    echo "Script updated successfully!"
-    ;;
-  help)
-    show_help
-    ;;
-  *)
-    check_for_updates
-    if [[ -n $1 ]] && [[ -n $2 ]]; then
-      # Check if the subdomain argument is provided
-      if [[ -n $3 ]]; then
-        # If the user provides a subdomain (third argument), use it
-        ssh -R $3:$1:localhost:$2 serveo.net
-      else
-        # If no subdomain is provided, just use port forwarding
-        ssh -R $1:localhost:$2 serveo.net
-      fi
-    else
+if [[ -n $1 ]] && [[ -n $2 ]]; then
+  # Use command with or without subdomain based on user input
+  if [[ -n $3 ]]; then
+    # With subdomain if provided
+    $ssh_command -R $3:$1:localhost:$2 serveo.net
+  else
+    # Without subdomain, just port forwarding
+    $ssh_command -R $1:localhost:$2 serveo.net
+  fi
+else
+  case "$1" in
+    update)
+      # Navigate to the script's directory and pull updates
+      cd "$(dirname "$0")" && git pull https://github.com/oyin25/serveo.git main
+      echo "Script updated successfully!"
+      ;;
+    help)
+      show_help
+      ;;
+    *)
       echo "Error: Incorrect usage."
       show_help
-      echo "For more information, type: ./hosted.sh help"
-    fi
-    ;;
-esac
+      ;;
+  esac
+fi
